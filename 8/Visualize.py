b5d082dc18b08f0bb5fad1456a5de61993d1c28a
tikdev_william <luisanalexander@unsrat.ac.id>
2019-08-13 10:44:14 +0800

    Menambahkan keyword dan bigram

---
+++ 8/Visualize.py
@@ -0,0 +1,35 @@
+import numpy as np 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class Visualize:
+    def main(self, comments_val, keywords_val, analyzed_val, tot_comments_val):
+        # suborn style
+        sns.set(style="darkgrid", context="paper", font_scale=1)
+        
+        # Set up the matplotlib figure
+        f, (ax1, ax2) = plt.subplots(2, 1, figsize=(6, 10), sharex=False)
+
+
+        # Grafik komentar
+        labels = 'Tidak terkategorikan', 'Mendukung', 'Tidak mendukung'
+        sizes = comments_val
+        colors = ['gold', 'yellowgreen', 'lightskyblue']
+        explode = (0.1, 0, 0)
+        ax1.pie(sizes, explode=explode, labels=labels, colors=colors,
+        autopct='%1.1f%%', shadow=True, startangle=140)
+        ax1.title.set_text("Analisis komentar video Kimi Hime\n" + "'DEAR BAPAK PRESIDEN JOKO WIDODO....'\n")
+
+        # Grafik keywords and bigram
+        x2 = ["K.Positive", "K.Negative", "B.Positive", "B.Negative"]
+        y2 = keywords_val
+        sns.barplot(x=x2, y=y2, palette="Blues_d", ax=ax2, linewidth=0)
+        ax2.axhline(0, color="k", clip_on=False)
+        ax2.set_ylabel("Keywords / Bigrams\n" + "(match)")
+        
+        # tampilkan grafik
+        plt.text(0.5, -2.5, "Teranalisa " + str(analyzed_val) + " dari "+ str(tot_comments_val) +" komentar")
+        plt.show()
+
+# if __name__ == "__main__":
+#     Visualize().main([2, 1, 3], [4, 2, 1, 3], 900)
---
+++ 8/resources/keywords.py
@@ -0,0 +1,27 @@
+# global variables
+keywords_positive = [
+    'langgeng', 'semangat', 'mantul', 'gemesin', 'natural',
+    'kepo', 'lucu', 'romantis', 'perfect', 'congrats',
+    'finally', 'terharu', 'merinding', 'sabar', 'smart',
+    'salut', 'santuy', 'mirip', 'bijak', 'ikhlas',
+    'hebat', 'cakep', 'kasihan', 'sopan', 'insaf', 'tobat', 'aman', 'introspeksi', 
+    'respect', 'relax', 'suka', 'like', 'berkarya', 'hargai', 'hikmah', 'bahagia',
+    'like', 'proses', 'gg', 'sukses', 'sirik', 'pray', 'barbar', 'luv', 'benar',
+    'penyanyi', 'positif', 'cobaan'
+]
+keywords_negative = [
+    'goblok', 'anjing', 'bodoh', 'bacot', 'kontol',
+    'sampah', 'lonte', 'munafik', 'anjir', 'hujat',
+    'ngentot', 'murahan', 'bangsat', 'coli', 'jelek',
+    'jijik', 'tolol', 'blok', 'toket', 'pamer',
+    'fitnah', 'hina', 'gila', 'bego', 'seksi', 
+    'hot', 'drama', 'lol', 'mampus', 'musang', 'montok', 'ngotot',
+    'preketek', 'telanjang', 'babi', 'kampungan', 'peang',
+    'sombong', 'glodok', 'bohong', 'menipu', 'maniac', 'gajelas', 'bego', 
+    'bacol', 'senonoh', 'gaje', 'dajjal', 'apes', 'hancur, ganggu', 'cengeng', 
+    'buaya', 'basi', 'gundek', 'kapok', 'sesat', 'aneh', 'tolah', 'mesum', 'pantek', 
+    'cupu', 'ceurik', 'halu', 'sinetron', 'ngeyel', 'coeg', 'colmek', 'lebay',
+    'karma', 'sokor', 'deportasi', 'miris', 'creeper', 'pascol', 'adult', 'bangke',
+    'lebok', 'comberan', 'pea', 'report', 'dungu', 'jancok', 'oon', 'victim', 
+    'heran', 'birahi', 'parah', 'nasib', 'aurat', 'tuntut', 'panas', 'paradoks', 'puki'
+]
